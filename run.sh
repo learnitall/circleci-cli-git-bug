7a4433256328495353fcd71bd7fe9d73f7f559f9
Ryan Drew <ryan.drew@isovalent.com>
2022-06-10 16:10:59 -0600

    Finish demo
    Signed-off-by: Ryan Drew <ryan.drew@isovalent.com>

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-set -eo pipefail
+set -xeo pipefail
 
 circleci version
 circleci config validate config.yml
+
+touch not_staged
+touch staged
+git add staged
+git ls-files
+
+ls
 circleci local execute -c config.yml --job list_files
+
+git restore --staged staged
+rm staged not_staged
 
